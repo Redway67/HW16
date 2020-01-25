93e652cde48c3cdeb0cb84ef372be0159da4eea4
Igor <redway@mail.ru>
2020-01-25 22:16:24 +0600

    HW16

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route("/contacts/")
+@app.route('/contacts/')
 def contacts():
     return render_template('contacts.html')
 
 
-@app.route("/search/")
+@app.route('/search/', methods=['POST'])
 def query():
     return render_template('search.html')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
