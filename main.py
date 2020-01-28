1ce13458447ac6ce34029e5fd6f961d70e482ac6
Igor <redway@mail.ru>
2020-01-28 23:02:28 +0600

    HW17

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template, request, flash
-import requests
+from flask import Flask, render_template, request
 import json
 
-DOMAIN = 'https://api.hh.ru/'
-URL_VACANCIES = f'{DOMAIN}vacancies'
-URL_AREA = f'{DOMAIN}suggests/areas'
+from modules.parser import parser
 
 app = Flask(__name__)
 
@@ -28,68 +25,8 @@ def search():
 
 @app.route('/search/', methods=['POST'])
 def results():
-    # TODO : вынести def results()  в отдельный модуль
-    req = []
-    skills = {}
-    total = 0
-    qnt_skills = 0
-    area = '1'  # по умолчанию Москва
-    vacancy = request.form['vacancy']
-    if not vacancy:
-        vacancy = 'python developer'  # по умолчанию ищем python developer
-    region = request.form['region']
-    # TODO заполнить info данными по умолчанию
     # TODO : необходимо запоминать дату и время на которое сделан запрос, ситуация ао вакансиям может измениться
-    info = {'region': region, 'vacancy': vacancy}  # передаем в html
-
-    # находим код региона для последущего запроса
-    # TODO Если region = Москва, то запрос не нужен
-    params = {'text': f'{region}'}
-    ra = requests.get(URL_AREA, params=params)
-    if ra.status_code == 200:
-        area_result = ra.json()
-        if area_result['items']:
-            area = area_result['items'][0]['id']
-        else:
-            print('Такого региона нет!')
-    else:
-        print('Ищем вакансию по умолчанию в Москве')
-
-    par = {'text': vacancy, 'area': area}
-    rv = requests.get(URL_VACANCIES, params=par)
-    if rv.status_code == 200:
-        result = rv.json()
-        found = result['found']
-        info['count'] = found
-        # идем по страницам и считаем навыки
-        # TODO : нужен прогресс-бар
-        if result['items']:
-            for p in range(1 + (found // 20)):
-                params = {'text': vacancy, 'area': area, 'page': p}
-                res = requests.get(URL_VACANCIES, params=params).json()
-                # страница {p}
-                for j in res['items']:
-                    resj = requests.get(j['url']).json()
-                    for i in resj['key_skills']:
-                        if i['name'] in skills:
-                            skills[i['name']] += 1
-                        else:
-                            skills.setdefault(i['name'], 1)
-                            qnt_skills += 1
-                        total += 1
-            # статистика
-            for k, v in sorted(skills.items(), key=lambda x: int(x[1]), reverse=True):
-                skill = {'name': k, 'count': v, 'percent': round(v / total * 100, 2)}  # в формате как в задании
-                req.append(skill)
-
-            info['requirement'] = req
-            # сохраняем в файл запрос и результат
-            with open('last_call.json', 'w', encoding='utf-8') as f:
-                json.dump(info, f, ensure_ascii=False)
-        else:
-            print(f'В регионе {region} вакансии {vacancy} не найдено.')
-    else:
-        print('Ошибка поиска!')
+    info = parser(request.form['vacancy'], request.form['region'])
     return render_template('result.html', info=info)
 
 
---
+++ modules/parser.py
@@ -0,0 +1,64 @@
+import requests
+import json
+
+DOMAIN = 'https://api.hh.ru/'
+URL_VACANCIES = f'{DOMAIN}vacancies'
+URL_AREA = f'{DOMAIN}suggests/areas'
+
+
+def parser(vacancy='Python developer', region='Москва'):
+    req = []
+    skills = {}
+    total = 0
+    qnt_skills = 0
+    # по умолчанию info
+    area = '1'  # код Москвы
+    info = {'region': region, 'vacancy': vacancy, 'found': 0, 'requirement': req}  # передаем в html
+
+    # находим код региона для последущего запроса
+    if region != 'Москва':
+        params = {'text': f'{region}'}
+        ra = requests.get(URL_AREA, params=params)
+        if ra.status_code == 200:
+            area_result = ra.json()
+            if area_result['items']:
+                area = area_result['items'][0]['id']
+            else:
+                print('Такого региона нет!')
+
+    par = {'text': vacancy, 'area': area}
+    rv = requests.get(URL_VACANCIES, params=par)
+    if rv.status_code == 200:
+        result = rv.json()
+        info['found'] = result['found']
+        # идем по страницам и считаем навыки
+        # TODO : нужен прогресс-бар
+        if result['items']:
+            for p in range(1 + (info['found'] // 20)):
+                params = {'text': vacancy, 'area': area, 'page': p}
+                res = requests.get(URL_VACANCIES, params=params).json()
+                # страница {p}
+                for j in res['items']:
+                    resj = requests.get(j['url']).json()
+                    for i in resj['key_skills']:
+                        if i['name'] in skills:
+                            skills[i['name']] += 1
+                        else:
+                            skills.setdefault(i['name'], 1)
+                            qnt_skills += 1
+                        total += 1
+            # статистика
+            for k, v in sorted(skills.items(), key=lambda x: int(x[1]), reverse=True):
+                skill = {'name': k, 'count': v, 'percent': round(v / total * 100, 2)}  # в формате как в задании
+                req.append(skill)
+
+            info['requirement'] = req
+            # сохраняем в файл запрос и результат
+            with open('last_call.json', 'w', encoding='utf-8') as f:
+                json.dump(info, f, ensure_ascii=False)
+        else:
+            print(f'В регионе {region} вакансии {vacancy} не найдено.')
+    else:
+        print('Ошибка поиска!')
+
+    return info
